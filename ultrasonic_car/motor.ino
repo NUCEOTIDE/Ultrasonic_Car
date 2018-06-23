a05c9e4a3a4534e4886b08140bca7b1ef12414c4
Jeff Dui <1174980556@qq.com>
2018-06-23 22:02:57 +0800

    Add files via upload

---
+++ ultrasonic_car/motor.ino
@@ -0,0 +1,16 @@
+//function that control all motors
+//#include <motor.h>
+class Motor{
+  public:
+  Motor(int motorPin){
+    initial(motorPin);
+  }
+  void initial(int motorPin){
+  pinMode(motorPin,OUTPUT);
+  }
+  private:
+  int motorPos;
+  int a;
+};
+
+
---
+++ ultrasonic_car/ultrasonic_car.ino
@@ -0,0 +1,11 @@
+#include <arduino.h>
+//main function for ultrasonic little car
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
